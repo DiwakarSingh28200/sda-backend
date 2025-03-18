22a89da689259351094fecd1fce11c92335d223c
vinaycreative <vnaycreative@gmail.com>
2025-03-18 22:09:36 +0530

    fix: re config set-cookies option

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import compression from "compression";
 import logger from "./config/logger";
 import cookieParser from "cookie-parser";
 import apiV1Routes from "./routes/v1"; 
-
+const allowedOrigins = [process.env.FRONTEND_URL || "http://localhost:3000"];
 
 const app: Application = express(); 
 app.use(cookieParser()); 
@@ -13,32 +13,12 @@ app.use(cookieParser());
 // Global Middleware
 app.use(express.json()); // Parse JSON requests
 app.use(express.urlencoded({ extended: true })); // Parse URL-encoded requests
-
-const setCorsHeaders = (req: Request, res: Response, next: NextFunction): void => {   
-  res.setHeader("Access-Control-Allow-Origin", process.env.FRONTEND_URL || "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-
-  if (req.method === "OPTIONS") {
-    res.sendStatus(204);
-    return;
-  }
-
-  next();
-};
-
-app.use(setCorsHeaders);
-
 app.use(cors({
-  origin: [
-    process.env.FRONTEND_URL || "http://localhost:3000", 
-  ], 
-  credentials: true, 
+  origin: allowedOrigins, 
+  credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
   allowedHeaders: ["Content-Type", "Authorization"], 
-
-  })
+})
 );
 app.use(helmet()); // Secure HTTP headers
 app.use(compression());
