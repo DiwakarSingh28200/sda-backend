cc36de57a0cd163338690e1ccbd29b0988a9dc80
vinaycreative <vnaycreative@gmail.com>
2025-03-19 02:01:47 +0530

    fix: cookies bug resolve

--- src/types/auth.ts
+++ src/types/auth.ts
@@ -1,10 +1,3 @@
 import { Request } from "express";
 
-export interface AuthRequest extends Request {
-  user: {
-    id: string;
-    name: string;
-    email: string;
-    department_id: string;
-  };
-}
+
