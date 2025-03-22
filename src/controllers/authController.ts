1e922a7e8007f92b73c49d304ad69e0b3d2ab3c6
vinaycreative <vnaycreative@gmail.com>
2025-03-22 10:41:22 +0530

    fix: cookie minor issue

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -101,12 +101,12 @@ export const loginEmployee = async (req: Request, res: Response<ApiResponse<any>
       httpOnly: true,
       secure: true, 
       sameSite: "none", 
-      domain: 'www.vinaydemos.site',
       maxAge: 30 * 24 * 60 * 60 * 1000, 
     }).json({
         success: true,
         message: "Login successful.",
         data: {
+          token: token,
           employee: {
             id: employee.id,
             employee_id: employee.employee_id,
