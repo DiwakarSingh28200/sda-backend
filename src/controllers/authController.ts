20f644f2737cd3c0dc422a53841e222c81c37ecf
vinaycreative <vnaycreative@gmail.com>
2025-03-22 16:34:25 +0530

    fix: cookies set bug resolve

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -99,7 +99,7 @@ export const loginEmployee = async (req: Request, res: Response<ApiResponse<any>
     httpOnly: true,
     secure: true, 
     sameSite: "lax", 
-    domain: ".vinaydemos.site", 
+    domain: process.env.COOKIE_DOMAIN, 
     maxAge: 30 * 24 * 60 * 60 * 1000, 
   }).json({
         success: true,
