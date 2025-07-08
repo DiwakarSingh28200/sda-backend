49093ae3a938f85f26111561715f40f1406c6584
Vinay <vnaycreative@gmail.com>
2025-07-08 20:21:36 +0530

    feat: change pdf generation method

--- src/modules/zoho/agreement/agreement.utils.ts
+++ src/modules/zoho/agreement/agreement.utils.ts
@@ -3,52 +3,72 @@ import path from "path"
 // import { chromium } from "playwright"
 import puppeteer from "puppeteer"
 
+import * as pdf from "html-pdf-node"
+
 export function renderTemplate(template: string, data: Record<string, string>) {
   return template.replace(/\{\{(.*?)\}\}/g, (_, key) => data[key.trim()] || "")
 }
 
-export const generateAgreementPDF = async (data: Record<string, string>): Promise<Uint8Array> => {
-  // const templatePath = path.join(__dirname, "./dealer-agreement.template.html")
+export async function generateAgreementPDF(data: Record<string, string>): Promise<any> {
   const templatePath = path.join(
     process.cwd(),
     "src",
     "template",
     "dealer-agreement.template.html"
   )
-
   const rawHtml = fs.readFileSync(templatePath, "utf-8")
-  const compiledHtml = renderTemplate(rawHtml, data)
 
-  // const browser = await chromium.launch({
-  //   headless: true,
-  // })
+  const compiledHtml = rawHtml.replace(/\{\{(.*?)\}\}/g, (_, key) => data[key.trim()] || "")
 
-  console.log("Puppeteer executable path:", puppeteer.executablePath())
+  const options = { format: "A4" }
+  const file = { content: compiledHtml }
 
-  const browser = await puppeteer.launch({
-    headless: true,
-    executablePath: puppeteer.executablePath(),
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-  })
-
-  const page = await browser.newPage()
-  await page.setContent(compiledHtml, { waitUntil: "domcontentloaded" })
-
-  const pdfBuffer = await page.pdf({
-    format: "A4",
-    printBackground: true,
-    margin: {
-      top: "20px",
-      right: "10px",
-      bottom: "40px",
-      left: "10px",
-    },
-  })
-
-  await browser.close()
-
+  const pdfBuffer = await pdf.generatePdf(file, options)
   return pdfBuffer
 }
+
+// export const generateAgreementPDF = async (data: Record<string, string>): Promise<Uint8Array> => {
+//   // const templatePath = path.join(__dirname, "./dealer-agreement.template.html")
+//   const templatePath = path.join(
+//     process.cwd(),
+//     "src",
+//     "template",
+//     "dealer-agreement.template.html"
+//   )
+
+//   const rawHtml = fs.readFileSync(templatePath, "utf-8")
+//   const compiledHtml = renderTemplate(rawHtml, data)
+
+//   // const browser = await chromium.launch({
+//   //   headless: true,
+//   // })
+
+//   console.log("Puppeteer executable path:", puppeteer.executablePath())
+
+//   const browser = await puppeteer.launch({
+//     headless: true,
+//     executablePath: puppeteer.executablePath(),
+//     args: ["--no-sandbox", "--disable-setuid-sandbox"],
+//   })
+
+//   const page = await browser.newPage()
+//   await page.setContent(compiledHtml, { waitUntil: "domcontentloaded" })
+
+//   const pdfBuffer = await page.pdf({
+//     format: "A4",
+//     printBackground: true,
+//     margin: {
+//       top: "20px",
+//       right: "10px",
+//       bottom: "40px",
+//       left: "10px",
+//     },
+//   })
+
+//   await browser.close()
+
+//   return pdfBuffer
+// }
 
 export interface ServiceRateCardItem {
   SERVICE_CATEGORY: string
